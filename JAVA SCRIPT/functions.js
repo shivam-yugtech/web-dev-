fba7250486a9ee84c19f262a4393d31e925e864f
shivam-yugtech <choudhary2002shivam@gmail.com>
2026-06-10 16:13:22 +0530

    started learning JS

---
+++ JAVA SCRIPT/functions.js
@@ -0,0 +1,62 @@
+// 3 types of function
+
+// 1. Normal Function
+
+//Function definition
+function function_name(par1,par2)
+{
+    //do something
+}
+
+// function invoke or call 
+// function_name(arg1,arg2);
+
+// function add(a,b)
+// {
+//     return a+b;
+// }
+
+// let ans = add(2,6);
+// console.log(ans);
+
+// function are treated as first clas citizens in javascript
+// -> functions can be returned 
+// -> functions can be passed as Parameters/argument 
+
+function Calculator(str,a,b)
+{
+    if(str == "add")
+    {
+        return function add(){
+            console.log(a + b);
+        }
+    }
+    else if(str == "sub")
+    {
+        return function sub(){
+            return a-b;
+        }    
+    }
+}
+
+let returnedfunc = Calculator('add',2,8);
+console.log("returned function is \n"+ returnedfunc);
+returnedfunc(); 
+
+
+// 2. Function Expression
+
+let variable_name = function (){
+    // do something
+}
+// name of a variable is used to invoke the function
+// variable_name();
+
+let sayHi = function (){
+    console.log("hello guys is am function Expression");
+}
+
+console.log("line 59\n" + sayHi);   
+sayHi();
+// abcd(); //error abcd is not defined 
+
--- JAVA SCRIPT/index.js
+++ JAVA SCRIPT/index.js
@@ -8,92 +8,112 @@ var a;      //int string float ?
 a = 10;
 //variable reinitialization
  a = 20;
-console.log(typeof a);
-console.log(a);
+// console.log(typeof a);
+// console.log(a);
 
 a="Hello";          // re initialization of variable 'a'
-console.log(a);
+// console.log(a);
 
 //typeof operator -> it tells us the type of data stored in a variable 
-console.log(typeof a);
+// console.log(typeof a);
 
 a= true;
-console.log(a);
-console.log(typeof a);
+// console.log(a);
+// console.log(typeof a);
 
 a= null;
-console.log(a);
-console.log(typeof a);   //bug in JS -> it should return null but it returns object
+// console.log(a);
+// console.log(typeof a);   //bug in JS -> it should return null but it returns object
 
 //Number
 var num = 10;
-console.log(num);
+// console.log(num);
 
 var float = 2.5;
-console.log(float);
+// console.log(float);
 
 //boolean
 var t = true;
 var f = false;
-console.log(t);
-console.log(f);
+// console.log(t);
+// console.log(f);
 
 //string
 //'', "", ``
 //single quotes, double quotes, backticks
 var str = 'a';
-console.log(typeof str);
+// console.log(typeof str);
 str = "how you doin?\ni am fine. what about you?";
-// backslash n(\n) -> new line
-console.log(str);
+// backslash n("\n") -> new line
+// console.log(str);
 
 var b = `hi hope you guys are 
 able to grasp the concepts`;
-console.log(b);
+// console.log(b);
 
 var num = 24356;
-console.log(`half of ${num} is ${num/2}`); 
+// ${}
+// console.log(`half of ${num} is ${num/2}`); 
 
 
 // var has some problem
 // 1. redeclaration is allowed
 var r = "hello";
-console.log(r);
+// console.log(r);
 
 var r = 100;
-console.log(r);
+// console.log(r);
 
 
-// isko overcome karne ke liye let and const aaye
+// isko overcome karne ke liye "let" and "const" aaye
 // let -> redeclaration not allowed, re initialization allowed
 let l = 200;
-console.log(l);
-
-// let l = "hello";   //SyntaxError: Identifier 'l' has already been declared
 // console.log(l);
 
-//we can re initialize a variable but we cannot redeclare it
+//  let l = "hello";   //SyntaxError: Identifier 'l' has already been declared
+// console.log(l);
+
+//we can reinitialize a variable but we cannot redeclare it
 l=300;
-console.log(l);
+// console.log(l);
 
 
 //loops in js
 var num = 10;
 
-for(var i=0; i < num;i++){
+// for(var i=0; i < num;i++){
+//     if(i%2==0){
+//         console.log("num is even");
+//     }
+// }
+
+// 2nd problem with var keyword
+// var keyword is function scoped in case of function else it is global scope 
+// let keyword is block scoped
+
+let j= 20;
+for( var i=0; i < num;i++){
+    let j=20;
     if(i%2==0){
-        console.log("num is even");
+        let hello = 1000;
+        console.log(i);
+        console.log(hello);
+        
     }
+    console.log("inner "+i);
 }
+console.log("outer "+j);
+console.log("value of i is "+ i);
+ console.log(hello);    // error dega hello is not defined
+ 
 
-// // const keyword -> redeclaration and re initialization both are not allowed
-// const a = 2;
-// console.log(a);
-//         //na hi redeclaration na initialization
-// //const a=5;
-// a=4;    //TypeError: Assignment to constant variable.
-// console.log("hey");
-
+// const keyword -> redeclaration and re initialization both are not allowed
+const n = 2;
+// console.log(n);
+        //na hi redeclaration na reinitialization
+//const a=5;
+//  n=4;    //TypeError: Assignment to constant variable.
+// console.log(n);
 
 
 
